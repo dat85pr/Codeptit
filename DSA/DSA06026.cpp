aa98d28f6e5eacf1f32bbf3e7c3e7cfbd20952fc
Hoang Dinh Dat <dathoangdinh11@gmail.com>
2025-03-13 11:49:15 +0700

    Create DSA06026.cpp

---
+++ DSA/DSA06026.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void testCase() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for (int &i : a) cin >> i;
+    for (int i = 1; i < n; ++i) {
+        bool is_sorted = true;
+        for (int j = 0; j < n - i; ++j) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+                is_sorted = false;
+            }
+        }
+        if (is_sorted) break;
+        cout << "Buoc " + to_string(i) + ": ";
+        for (int j = 0; j < n; ++j) {
+            cout << a[j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+
+        testCase();
+        cout << "\n";
+
+    return 0;
+}
