e6323f6e9e39555fdf5af37ceda6106a5339770b
dat85pr <dathoangdinh11@gmail.com>
2025-08-08 22:21:13 +0700

    Auto commit changes

---
+++ CodePTIT - C/C07016 - Sắp xếp thí sinh.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdint.h>
+#include<math.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include<ctype.h>
+struct SinhVien{
+	int ma;
+	char ten[100];
+	char ns[100];
+	double d1, d2, d3;
+	double tong;
+};
+typedef struct SinhVien SV;
+void nhap(SV *a){
+	getchar();
+	gets(a->ten);
+	gets(a->ns);
+	scanf("%lf %lf %lf", &a->d1, &a->d2, &a->d3);
+}
+void in(SV a){
+	printf("%d %s %s %.1lf\n", a.ma, a.ten, a.ns, a.tong);
+}
+int cmp(const void *a, const void *b){
+	SV *x = (SV*)a;
+	SV *y = (SV*)b;
+	return x->tong < y->tong;
+}
+int main(){
+	int n;
+	scanf("%d", &n);
+	SV a[n];
+	for(int i = 0; i < n; i++){
+		a[i].ma = i + 1;
+		nhap(&a[i]);
+		a[i].tong = a[i].d1 + a[i].d2 + a[i].d3;
+	}
+	qsort(a, n, sizeof(SV), cmp);
+	for(int i = 0; i < n; i++){
+		in(a[i]);
+	}
+}
