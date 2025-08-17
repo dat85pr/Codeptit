172470440a216be538cf6e9aa04a8dee7d29533c
dat85pr <dathoangdinh11@gmail.com>
2025-08-17 11:20:58 +0700

    Auto commit changes

---
+++ CodePTIT - Python/PY01033 - Bộ ba nguyên tố cùng nhau.py
@@ -0,0 +1,13 @@
+from math import *
+l, r = map(int, input().split())
+for i in range(l, r - 1):
+    for j in range(i + 1, r):
+        for k in range(j + 1, r + 1):
+            if gcd(i, j) == 1 and gcd(j, k) == 1 and gcd(i, k) == 1:
+                print('(', end='')
+                print(i, end = '')
+                print(', ', end = '')
+                print(j, end = '')
+                print(', ', end = '')
+                print(k, end = '')
+                print(')')
