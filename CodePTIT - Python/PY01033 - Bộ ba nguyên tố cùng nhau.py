8a5d266b76bb43c849128d0bcfdb4b405e663f81
dat85pr <dathoangdinh11@gmail.com>
2025-08-17 11:22:46 +0700

    update

--- CodePTIT - Python/PY01033 - Bộ ba nguyên tố cùng nhau.py
+++ CodePTIT - Python/PY01033 - Bộ ba nguyên tố cùng nhau.py
@@ -4,10 +4,4 @@ for i in range(l, r - 1):
     for j in range(i + 1, r):
         for k in range(j + 1, r + 1):
             if gcd(i, j) == 1 and gcd(j, k) == 1 and gcd(i, k) == 1:
-                print('(', end='')
-                print(i, end = '')
-                print(', ', end = '')
-                print(j, end = '')
-                print(', ', end = '')
-                print(k, end = '')
-                print(')')
+                print('(' + str(i) + ', ' + str(j) + ', ' + str(k) + ')')
