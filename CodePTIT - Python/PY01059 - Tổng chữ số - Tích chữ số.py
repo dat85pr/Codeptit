b604a8c5627820adf94ec2f6af4d715d42ac7ca5
dat85pr <dathoangdinh11@gmail.com>
2025-08-18 08:14:43 +0700

    Auto commit changes

---
+++ CodePTIT - Python/PY01059 - Tổng chữ số - Tích chữ số.py
@@ -0,0 +1,15 @@
+from math import *
+for t in range(int(input())):
+    n = input()
+    tong = 0
+    tich = 0
+    for i in range(len(n)):
+        m = ord(n[i]) - ord('0')
+        if i % 2 == 0:
+            tong += m
+        else:
+            if tich == 0:
+                tich = m
+            elif m != 0:
+                tich *= m
+    print(tong, tich)
