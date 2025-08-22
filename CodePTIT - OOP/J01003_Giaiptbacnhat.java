1d9c4c1edc6dae4513b98af0e521f1324b8530ba
dat85pr <dathoangdinh11@gmail.com>
2025-08-22 21:59:33 +0700

    Auto commit changes

---
+++ CodePTIT - OOP/J01003_Giaiptbacnhat.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class J01003_Giaiptbacnhat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("VSN");
+            } else {
+                System.out.println("VN");
+            }
+        } else {
+            System.out.printf("%.2f", -b / a);
+        }
+    }
+
+}
