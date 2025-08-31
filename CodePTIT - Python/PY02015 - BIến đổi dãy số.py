febeeee0527a2a8ae1922716a698f981fafcd6d2
dat85pr <dathoangdinh11@gmail.com>
2025-08-31 20:34:07 +0700

    Auto commit changes

---
+++ CodePTIT - Python/PY02015 - BIến đổi dãy số.py
@@ -0,0 +1,13 @@
+from math import *
+while True:
+    a = [int(i) for i in input().split()]
+    if a.count(0) == 4:
+        break
+    cnt = 0
+    while a.count(a[0]) != 4:
+        cnt += 1
+        tmp = a.copy()
+        for i in range(4):
+            a[i] = abs(tmp[i] - tmp[(i + 1) % 4])
+    print(cnt)
+
