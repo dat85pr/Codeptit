8a23a9b252e93b2aafa8f0a3a050b8f83852b403
dat85pr <dathoangdinh11@gmail.com>
2025-09-11 09:48:38 +0700

    update

--- CodePTIT - OOP/java1.java
+++ CodePTIT - OOP/java1.java
@@ -1,7 +1,15 @@
-
+import java.util.*;
 public class java1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        Arrays.sort(a);
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
     }
 }
-
