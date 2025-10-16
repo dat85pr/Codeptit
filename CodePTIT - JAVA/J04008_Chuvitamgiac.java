1133f0741f9989ae4ff88ee85762394cad2d5dd1
dat85pr <dathoangdinh11@gmail.com>
2025-10-16 09:40:37 +0700

    Auto commit changes

---
+++ CodePTIT - JAVA/J04008_Chuvitamgiac.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import static java.lang.Math.*;
+class Point{
+    private double x, y;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Point(){
+        this(0, 0);
+    }
+    public Point(Point p){
+        this.x = p.x;
+        this.y = p.y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    public double distance(Point p){
+        return sqrt(pow(this.x - p.x, 2) + pow(this.y - p.y, 2));
+    }
+    public double distance(Point p1, Point p2){
+        return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+    }
+    @Override
+    public String toString(){
+        return x + " " + y;
+    }
+}
+
+public class J04008_Chuvitamgiac {
+    public static Scanner ip = new Scanner(System.in);
+    public static void main(String[] args){
+        int t = ip.nextInt();
+        for(int i = 0; i < t; i++){
+            Point a = new Point(ip.nextDouble(), ip.nextDouble());
+            Point b = new Point(ip.nextDouble(), ip.nextDouble());
+            Point c = new Point(ip.nextDouble(), ip.nextDouble());
+            double ab = a.distance(b);
+            double ac = a.distance(c);
+            double bc = b.distance(c);
+            if(ab + ac <= bc || ab + bc <= ac || bc + ac <= ab){
+                System.out.println("INVALID");
+            }
+            else{
+                System.out.println(String.format("%.3f", ab + bc + ac));
+            }
+        }
+    }
+}
